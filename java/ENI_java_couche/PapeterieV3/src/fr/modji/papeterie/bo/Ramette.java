322d4d7f43fb9574931340ff801549e9d6c5baea
Modji <jeanbaptiste.cochinard@gmail.com>
2021-05-11 06:59:08 +0200

    ADD PapeterieV3 init

---
+++ java/ENI_java_couche/PapeterieV3/src/fr/modji/papeterie/bo/Ramette.java
@@ -0,0 +1,35 @@
+package fr.modji.papeterie.bo;
+
+public class Ramette extends Article{
+    private int grammage;
+    public static String TYPE = "Stylo";
+
+    public Ramette(int idArticle, String marque, String ref, String designation, float pu, int qte, int grammage) {
+        super(idArticle, marque, ref, designation, pu, qte);
+        this.grammage = grammage;
+    }
+
+    public Ramette(String marque, String ref, String designation, float pu, int qte, int grammage) {
+        super(marque, ref, designation, pu, qte);
+        this.grammage = grammage;
+    }
+
+    @Override
+    public String toString() {
+        return "Ramette{" +
+                "idArticle=" + idArticle +
+                ", reference='" + reference + '\'' +
+                ", marque='" + marque + '\'' +
+                ", designation='" + designation + '\'' +
+                ", prixUnitaire=" + prixUnitaire +
+                ", qteStock=" + qteStock +
+                ", grammage=" + grammage +
+                '}';
+    }
+
+    // Getter & Setter
+    public int getGrammage() { return grammage; }
+    public void setGrammage(int grammage) {
+        this.grammage = grammage;
+    }
+}
