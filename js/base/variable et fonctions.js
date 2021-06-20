dbf8a1857e17a79356329719185e5a3dc3ba2347
Modji <jeanbaptiste.cochinard@gmail.com>
2021-06-20 08:56:09 +0200

    UPDATE base practice

--- js/base/variable et fonctions.js
+++ js/base/variable et fonctions.js
@@ -1,18 +1,19 @@
 /**
  * Demonstration utilisation variable, function et particularité == ===
  */
-var unNombre = 2;
+let varLimite = 2;  //variable de porté limite au bloc
+var unNombre = 2;   //variable de porté global
 var sonDouble = doubler(unNombre);
 console.log("le double de", unNombre, "est" , sonDouble);
 
 // == et ===
 var uneValeur = "3";    //type string
-console.log("valeur == 3", uneValeur == 3);
-console.log("valeur === 3", uneValeur === 3);
+console.log("valeur == 3", uneValeur == 3); //true
+console.log("valeur === 3", uneValeur === 3); //false
 
 //undefined
 var uneVariableNonDeclarer;
-console.log("ma var vaut '%s' dans le programme",uneVariableNonDeclarer);
+console.log("ma var vaut '%s' dans le programme", uneVariableNonDeclarer); // undefined
 
 // affiche une variable qui n'a pas été déclarer avec var et est donc global
 attention();
@@ -26,3 +27,16 @@ function doubler(param){
 function attention(){
     valeurTest = "oulala";
 }
+
+//Objet disponible : Math
+var nbAleatoire = Math.random()*100;
+console.log(nbAleatoire);
+// convertir en valeur entiere : méthode global
+nbAleatoire = parseInt(nbAleatoire);
+console.log(nbAleatoire);
+
+//Gestion des date
+var noel2018 = new Date(2018,11,25); // 11 = decembre
+console.log(noel2018);
+console.log(noel2018.toLocaleDateString());
+console.log(noel2018.getDate(),noel2018.getMonth(), noel2018.getDay());
